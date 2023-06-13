c61e9e00c18d16c7fc2eec5aa2c7a5f89e116af5
AyselAmrahova <emrahovaays@gmail.com>
2023-06-13 11:06:17 +0500

    nav responsive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './_navbarStyle.scss'
-
+import { DownOutlined } from '@ant-design/icons';
+import { Dropdown, Space, Typography } from 'antd';
+import { Link } from 'react-router-dom';
 export default function Navbar() {
+    const items = [
+        {
+            key: '1',
+            label: <Link to=''> AZE </Link>,
+        },
+        {
+            key: '2',
+            label: <Link to=''> ENG </Link>,
+        },
+        {
+            key: '3',
+            label: <Link to=''> RUS </Link>,
+        },
+    ];
     return (
         <>
             {/* bu divdə slider də olmalıdı */}
@@ -16,22 +31,46 @@ export default function Navbar() {
                             <p> Sifariş üçün </p>
                             <h6>*2121</h6>
                         </div>
-                        <ul>
+                        <ul className='navUl'>
                             <li><h5><Link to='/home' className='navLink'>Ana səhifə</Link></h5></li>
                             <li><h5><Link to='/products' className='navLink'>Məhsullar</Link></h5></li>
                             <li><h5><Link to='/about' className='navLink'>Haqqımızda</Link></h5></li>
                             <li><h5><Link to='/contact' className='navLink'>Əlaqə</Link></h5></li>
                         </ul>
-                        <div style={{ display: 'flex', margin: '0 auto' }}>
-                            <div className='navUser' style={{ border: " 1px solid #cedef3", borderRadius: '0.2rem', padding:'0.5rem 1rem'}}>
-                                <img src="https://slavyanka.az/static/media/PersonLight.8bad44e1.svg" alt="Example" />
+                        <div className='navIcon'>
+                            <div className='navLang'>
+                                <Dropdown
+                                    className='navDropDown'
+                                    menu={{
+                                        items,
+                                        selectable: true,
+                                        defaultSelectedKeys: ['1'],
+                                    }}
+                                >
+                                    <Typography.Link>
+                                        <Space style={{ color: '#f8f9fa ' }}>
+                                            AZE
+                                            <DownOutlined />
+                                        </Space>
+                                    </Typography.Link>
+                                </Dropdown>
                             </div>
-                            <div>
-                                <img src="https://slavyanka.az/static/media/ShoppingLight.8e403ab1.svg" alt="Example" />
+                            <div className='navUser'>
+                                <div className='navUserIcon'>
+                                    <img src="https://slavyanka.az/static/media/PersonLight.8bad44e1.svg" alt="Example" width="18" height="18" />
+                                </div>
                             </div>
+                            <div className='navBasket'>
+                                <div className='navBasketIcon'>
+                                    <img src="https://slavyanka.az/static/media/ShoppingLight.8e403ab1.svg" alt="Example" />
+                                </div>
+                            </div>
+                            <div className='navHamburger'>
+                                <img src="https://slavyanka.az/static/media/togglewhite.61779455.svg" alt="Example" />
+                            </div>
+
                         </div>
-                        {/* <img src="https://slavyanka.az/static/media/togglewhite.61779455.svg" alt="Example" /> */}
-                        <div>
+                        <div className='navCall'>
                             <img src="https://slavyanka.az/static/media/PhoneLight.b5e0dced.svg" alt="Example" />
                             <h6> Zəng sifarişi </h6>
                         </div>
