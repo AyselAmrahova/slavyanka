74c19719e4deb08f73f680e4d91950470a5865ce
AyselAmrahova <emrahovaays@gmail.com>
2023-06-14 10:29:26 +0500

    multer

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+    console.log(`App listening on PORT: ${process.env.PORT}`);
+})
+
+mongoose.connect(process.env.DB_CONNECTION).then(() => {
+    console.log('Mongo DB Connected!');
+}).catch((err) => {
+    console.log(err);
+})
+const SlidersSchema = new mongoose.Schema({
+    name: String,
+    url: String
+})
+
+const SliderModel = new mongoose.model('Sliders', SlidersSchema);
+
+
+app.get('/sliders', async (req, res) => {
+    const sliders = await SliderModel.find();
+    res.status(200).send({
+        message: 'data get success!',
+        data: sliders
+    })
+})
+app.post('/sliders', async (req, res) => {
+    const { name, url } = req.body;
+    const newSlider = new SliderModel({
+        name: name,
+        url: url
+    })
+    await newSlider.save();
+    res.status(201).send('slider posted successfully!')
+})
+
--- frontend/src/pages/Main/About/About.jsx
+++ frontend/src/pages/Main/About/About.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function About() {
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [images, setImages] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  function upload() {
+    setLoading(true)
+    const formData = new FormData();
+    formData.append("file", selectedImage);
+    formData.append("upload_preset", "zshz34od");
+    axios.post('https://api.cloudinary.com/v1_1/db9r6mgrx/image/upload', formData)
+      .then((res) => {
+        setImages([...images, res.data.secure_url])
+        console.log(res);
+        setLoading(false)
+      });
+  }
   return (
-    <div>About</div>
+    <>
+      {loading ? <div>loading...</div> : <>
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          upload()
+        }}>
+          <input onChange={(e) => {
+            setSelectedImage(e.target.files[0])
+          }} type='file' />
+          <button type='submit'>Add</button>
+        </form>
+        <ul>
+          {images && images.map((image, idx) => {
+            return <li key={idx}>
+              <img width={100} height={100} src={image} alt="" />
+            </li>
+          })}
+        </ul>
+      </>}
+    </>
   )
 }
