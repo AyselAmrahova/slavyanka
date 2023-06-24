0f374a35af7ef00357b62020adaa170d5dfca757
AyselAmrahova <emrahovaays@gmail.com>
2023-06-24 18:01:22 +0500

    Suggestion sect (used emailjs/browser)

--- frontend/src/pages/Main/Contact/Suggestions.jsx
+++ frontend/src/pages/Main/Contact/Suggestions.jsx
@@ -1,32 +1,62 @@
 import React from 'react'
+import { useRef } from 'react'
 import { Button, TextField } from '@mui/material';
 import './_suggestions.scss'
+import emailjs from "@emailjs/browser";
 
 
 export default function Suggestions() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_71iykcp",
+        "template_xq3g5un",
+        form.current,
+        "VAT0bKgAVTIR1F0JD"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <>
       <main>
-        <form>
+        <form
+          ref={form}
+          onSubmit={sendEmail}>
           <div className='suggestions-text'>
             <p>Təklif və iradlarını bildir!</p>
             <p className='suggestions-text-2p'>Müştərilərimizin düşüncələri önəmlidir</p>
           </div>
           <div className='input-div'>
-            <TextField label="Adınız"
+            <TextField
+              label="Adınız"
               className="form-input"
-              type="text" />
+              type="text"
+              name="user_name" />
           </div>
           <div className='input-div' >
-            <TextField label='Soyadınız'
+            <TextField
+              label='Soyadınız'
               className="form-input"
               type='text' />
           </div>
           <div className='input-div'>
             <TextField
-              placeholder='xxxxxxxxx'
+              label="Email"
               className="form-input"
-              type='text'
+              type="email"
+              name="user_email"
             />
           </div>
           <div className='input-div'>
@@ -41,9 +71,10 @@ export default function Suggestions() {
           </div>
           <div className='input-div'>
             <Button
+              type="submit"
+              value="Send"
               className='input-div-Button'
               variant="contained"
-              type="submit"
             >Göndər</Button>
           </div>
         </form>
